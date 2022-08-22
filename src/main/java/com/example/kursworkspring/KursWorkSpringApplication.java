e258ac1390de4bd2557b02ceabe24521cc16e3e8
abdullinru <abdullin17021989@gmail.com>
2022-08-22 16:39:51 +0600

    Initial commit

---
+++ src/main/java/com/example/kursworkspring/KursWorkSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.kursworkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursWorkSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursWorkSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursworkspring/KursWorkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursworkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursWorkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
