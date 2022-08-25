3bdc17403da2e77a9827ccfefccae37d441feac4
abdullinru <abdullin17021989@gmail.com>
2022-08-25 21:47:49 +0600

    тест для ExaminerServiceImpl

--- src/test/java/com/example/kursworkspring/service/ExaminerServiceImplTest.java
+++ src/test/java/com/example/kursworkspring/service/ExaminerServiceImplTest.java
@@ -31,16 +31,14 @@ class ExaminerServiceImplTest {
     Question q3Math = new Question("Math3", "Answer3");
     Question q4Math = new Question("Math4", "Answer4");
     Question q5Math = new Question("Math5", "Answer5");
-    Set<Question> questionsExample3Java, questionsExample5Java, questionsExample3Math, questionsExample5Math;
+    Set<Question> questionsExample5Java, questionsExample5Math, questionsAll;
     @Mock
-    private QuestionService javaQuestionServiceMock;
-
+    private JavaQuestionService javaQuestionServiceMock;
     @Mock
-    private QuestionService mathQuestionServiceMock;
+    private MathQuestionService mathQuestionServiceMock;
 
-    @Mock
-    private Random randomMock;
-
+//    @Mock
+//    private Random randomMock;
     @InjectMocks
     private ExaminerServiceImpl out;
 
@@ -48,6 +46,9 @@ class ExaminerServiceImplTest {
     public void init() {
         questionsExample5Java = new HashSet<>(Set.of(q1Java,q2Java,q3Java,q4Java,q5Java));
         questionsExample5Math = new HashSet<>(Set.of(q1Math,q2Math,q3Math,q4Math,q5Math));
+        questionsAll = new HashSet<>(Set.of(q1Java,q2Java,q3Java,q4Java,q5Java, q1Math,q2Math,q3Math,q4Math,q5Math));
+
+
         Mockito.when(javaQuestionServiceMock.getAll()).thenReturn(questionsExample5Java);
         Mockito.when(mathQuestionServiceMock.getAll()).thenReturn(questionsExample5Math);
 
@@ -60,10 +61,11 @@ class ExaminerServiceImplTest {
                 .thenReturn(q1Java,q5Java,q3Java,q1Java,q5Java, q4Java,q2Java,q4Java);
         Mockito.when(mathQuestionServiceMock.getRandom())
                 .thenReturn(q2Math,q3Math,q4Math, q1Math,q5Math, q1Math,q5Math,q2Math,q3Math,q4Math);
-        Mockito.when(randomMock.nextInt(anyInt()))
-                .thenReturn(0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
-        Assertions.assertThat(out.getQuestions(5))
-                .containsExactlyInAnyOrder(q1Java, q2Math, q5Java, q3Math, q3Java);
+//        Mockito.when(randomMock.nextInt(anyInt()))
+//                .thenReturn(0, 1, 0, 1, 0, 1, 0, 1, 0, 1);
+        Assertions.assertThat(out.getQuestions(10))
+                .containsExactlyInAnyOrder(q1Java, q2Java, q3Java, q4Java, q5Java, q1Math, q2Math, q3Math, q4Math, q5Math);
+//                .containsExactlyInAnyOrder(q1Java, q2Math, q5Java, q3Math, q3Java);
 //        Assertions.assertThat(out.getQuestions(5)).containsExactlyInAnyOrder(q1,q2,q3);
         Assertions.assertThatExceptionOfType(ExceptionIncorrectAmount.class)
                 .isThrownBy(() -> out.getQuestions(-5));
